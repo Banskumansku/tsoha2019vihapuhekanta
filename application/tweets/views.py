e1f0bc5a6ddf56011598180430ca0a2bff5fb15f
Banskumansku <jan.lindholm@helsinki.fi>
2019-03-19 23:25:13 +0200

    D for delete (CRD)
    Added way to delete tweets

--- application/tweets/views.py
+++ application/tweets/views.py
@@ -12,6 +12,14 @@ def tweets_index():
 def tweets_form():
     return render_template("tweets/new.html")
 
+@app.route("/delete", methods=["POST"])
+def delete():
+    tweetid = request.form.get("tweetid")
+    tweet = Tweet.query.filter_by(tweetid=tweetid).first()
+    db.session.delete(tweet)
+    db.session.commit()
+    return redirect(url_for("tweets_index"))
+
 
 @app.route("/tweets/", methods=["POST"])
 def tweets_create():
