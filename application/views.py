f173548cadb1c344bc7580c6d0fb4c1d06799481
Banskumansku <jan.lindholm@helsinki.fi>
2019-04-03 23:36:55 +0300

    Potential fix for heroku

--- application/views.py
+++ application/views.py
@@ -8,15 +8,15 @@ from application.tweets.models import Tweet
 @app.route("/")
 @login_required
 def index():
-    tweetquery = db.session().query(Tweet).filter(Tweet.account_id.contains(current_user.id))
+    tweetquery = db.session().query(Tweet).filter(Tweet.account_id == current_user.id)
     tweetSize = tweetquery.all()
-    tweetquery = db.session().query(Tweet).filter(Tweet.account_id.contains(current_user.id),
+    tweetquery = db.session().query(Tweet).filter((Tweet.account_id == current_user.id),
                                                   Tweet.tweettype.contains("normal"))
     normal_size = tweetquery.all()
-    tweetquery = db.session().query(Tweet).filter(Tweet.account_id.contains(current_user.id),
+    tweetquery = db.session().query(Tweet).filter((Tweet.account_id == current_user.id),
                                                   Tweet.tweettype.contains("offensive"))
     offensive_size = tweetquery.all()
-    tweetquery = db.session().query(Tweet).filter(Tweet.account_id.contains(current_user.id),
+    tweetquery = db.session().query(Tweet).filter((Tweet.account_id == current_user.id),
                                                   Tweet.tweettype.contains("hateful"))
     hateful_size = tweetquery.all()
     return render_template("index.html", tweets=len(tweetSize), tweetsNormal=len(normal_size),
