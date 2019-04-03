82e43df42abe8a8d3b4b78bf3d27141777463033
Banskumansku <jan.lindholm@helsinki.fi>
2019-04-03 18:40:03 +0300

    Heroku fix number n

--- application/views.py
+++ application/views.py
@@ -1,13 +1,14 @@
-from flask import render_template, redirect, url_for
+from flask import render_template
 from flask_login import login_required, current_user
 
-from application import app, db, api
+from application import app, db
 from application.tweets.models import Tweet
 
 
 @app.route("/")
 @login_required
 def index():
+    """
     tweetquery = db.session().query(Tweet).filter(Tweet.account_id.contains(current_user.id))
     tweetSize = tweetquery.all()
     tweetquery = db.session().query(Tweet).filter(Tweet.account_id.contains(current_user.id),
@@ -21,3 +22,5 @@ def index():
     hateful_size = tweetquery.all()
     return render_template("index.html", tweets=len(tweetSize), tweetsNormal=len(normal_size),
                            tweetsOffensive=len(offensive_size), tweetsHateful=len(hateful_size))
+    """
+    return render_template("index.html")
